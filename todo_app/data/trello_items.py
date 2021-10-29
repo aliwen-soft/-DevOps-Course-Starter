65a65351206fe2e0c590cee056bff08c29a0e95f
Alice <aliwen@softwire.com>
2021-10-29 17:23:06 +0100

    M2: some error handerling at API level (not at app level)

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,6 +1,7 @@
 from requests import get, put, post, delete
 from todo_app.flask_config import Config
-from todo_app.data.todo_item import TodoItem, TODO_STATUS, DOING_STATUS, DONE_STATUS
+from todo_app.data.todo_item import TodoItem, TODO_STATUS
+from todo_app.api_exception import ApiException
 
 TRELLO_BASE_URL="https://api.trello.com"
 
@@ -22,14 +23,18 @@ def get_items():
 
     r=get(url=url, params=params)
 
-    response = r.json()
+    if r.status_code == 200:
 
-    todo_items = map(
-        lambda response_item: TodoItem(response_item.get("id"), response_item.get("name"),  get_status_from_response_item(response_item)),
-        response
-        )
-   
-    return todo_items
+        response = r.json()
+
+        todo_items = map(
+            lambda response_item: TodoItem(response_item.get("id"), response_item.get("name"),  get_status_from_response_item(response_item)),
+            response
+            )
+    
+        return todo_items
+    
+    raise ApiException(r.status_code,"There was an issue while trying to get items")
 
 def get_item(id):
     """
@@ -42,8 +47,14 @@ def get_item(id):
         item: The saved item, or None if no items match the specified ID.
     """
     items = get_items()
-    return next((item for item in items if item['id'] == int(id)), None)
 
+    item = next((item for item in items if item['id'] == int(id)), None)
+
+    if item:
+        return item
+
+    raise ApiException(404 ,"That item does not exist")
+    
 def change_item_status(id, new_status):
     """
     Changes the item with specified id to specified status 
@@ -59,6 +70,8 @@ def change_item_status(id, new_status):
     update_card_params = {**auth_payload , "idList":list_id } 
 
     r=put(url=update_card_url, params=update_card_params)
+    if r.status_code != 200:
+        raise ApiException(r.status_code,"There was an while trying to change item status")
     
 def add_item(title):
     """
@@ -70,17 +83,20 @@ def add_item(title):
     Returns:
         item: The saved item.
     """
-    todo_list_Id = get_list_id_with_name(TODO_STATUS)
+    todo_list_Id = get_list_id_with_list_name(TODO_STATUS)
 
     make_card_url = TRELLO_BASE_URL+ "/1/cards/"
     make_cards_params = {**auth_payload , "name":title, "idList": todo_list_Id } 
 
     r=post(url=make_card_url, params=make_cards_params)
-    print(r)
+    
+    if r.status_code == 200:
 
-    respose_item= r.json()
+        respose_item = r.json()
 
-    return TodoItem(respose_item.get("id"), respose_item.get("name"))
+        return TodoItem(respose_item.get("id"), respose_item.get("name"))
+
+    raise ApiException(r.status_code,"There was an issue while trying to create an item")
 
 def remove_item(id):
     """
@@ -91,6 +107,8 @@ def remove_item(id):
     """
     delete_card_url = TRELLO_BASE_URL+ "/1/cards/" + id
     r = delete(url=delete_card_url, params=auth_payload)
+    if r.status_code != 200:
+        raise ApiException(r.status_code,"There was an error while trying to remove card: "+id)
 
 def get_status_from_response_item(item):
     id_list = item.get("idList")
