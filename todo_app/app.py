2fe1cbf47bfa7705c09717890dbf2b76ab98b58b
Alice <aliwen@softwire.com>
2021-11-30 11:51:32 +0000

    M3: complete minimum requirment

--- todo_app/app.py
+++ todo_app/app.py
@@ -7,39 +7,40 @@ from todo_app.view_model import ViewModel
 def create_app():
 
     app = Flask(__name__)
-    app.config.from_object(Config())
+    config = Config()
+    app.config.from_object(config)
 
 
     @app.route('/')
     def index():
-        items = get_items()
+        items = get_items(config)
         items = sorted(items, key = lambda item: item.get_ordering_index())
         item_view_model = ViewModel(items)
         return render_template("index.html", item_view_model = item_view_model)
 
     @app.route('/add-todo', methods=['POST'])
     def add_todo():
-        add_item(request.form.get("title"))
+        add_item(config, request.form.get("title"))
         return redirect("/")
 
     @app.route('/remove-todo/<id>', methods=['POST'])
     def remove_todo(id):
-        remove_item(id)
+        remove_item(config,id)
         return redirect("/")
 
     @app.route('/mark-complete/<id>', methods=['POST'])
     def mark_complete(id):
-        change_item_status(id, DONE_STATUS)
+        change_item_status(config, id, DONE_STATUS)
         return redirect("/")
 
     @app.route('/mark-doing/<id>', methods=['POST'])
     def mark_doing(id):
-        change_item_status(id, DOING_STATUS)
+        change_item_status(config, id, DOING_STATUS)
         return redirect("/")
 
     @app.route('/mark-todo/<id>', methods=['POST'])
     def mark_todo(id):
-        change_item_status(id, TODO_STATUS)
+        change_item_status(config, id, TODO_STATUS)
         return redirect("/")
 
     return app
