2bc55657189bce24ed07f366ec3580d4685d3cab
Alice <aliwen@softwire.com>
2022-09-06 10:23:53 +0100

    M19: got tests Working

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -4,9 +4,9 @@ import os
 class Config:
     def __init__(self):
         """Base configuration variables."""
-        
-        self.DB_CONNECTION_STRING = os.environ.get('TODO_CONNECTION_STRING')
 
+        self.DB_CONNECTION_STRING = os.environ.get('TODO_CONNECTION_STRING')
+     
         self.DB_NAME = os.environ.get('TODO_DB_NAME')
 
         if not (self.DB_CONNECTION_STRING and self.DB_NAME):
--- todo_app/tests/test_app.py
+++ todo_app/tests/test_app.py
@@ -1,57 +1,28 @@
-import os
 import pytest
 from dotenv import load_dotenv, find_dotenv
-import requests
 from todo_app import app
+import mongomock
+import os
+
 
 @pytest.fixture
-def client():
+def client(monkeypatch):
     filepath = find_dotenv(".env.test")
     load_dotenv(filepath, override=True)
+    monkeypatch.setenv('TODO_CONNECTION_STRING', 'fakemongo.com:27017')
 
-    test_app = app.create_app()
+    with mongomock.patch(servers=(('fakemongo.com', 27017),)):
+        test_app = app.create_app()
+        with test_app.test_client() as client:
+            yield client
 
-    with test_app.test_client() as client:
-        yield client
 
-def test_index_page(monkeypatch, client):
-    monkeypatch.setattr(requests,'get', mock_get)
+def test_index_with_added_card(client):
+    client.post('/add-todo', data=dict(title="I am a card"))
     response = client.get('/')
 
     html_response = response.data.decode()
-    
+
     assert response.status_code == 200
     assert 'I am a card' in html_response
     assert 'Mark in progress' in html_response
- 
-class StubResponse(): 
-    def __init__(self, fake_response_data, status_code): 
-        self.fake_response_data = fake_response_data 
-        self.status_code = status_code
-    def json(self): 
-        return self.fake_response_data
-
-
-def get_cards_stub(): 
-    fake_response_data = [{ 
-        'id': '123abc', 
-        'name': 'I am a card', 
-        'idList': 'lista',
-        'dateLastActivity' : '2021-11-29T18:00:00.00Z'
-    }] 
-    return StubResponse(fake_response_data, 200)
-
-def get_list_stub(): 
-    fake_response_data = { 
-        'id': 'lista', 
-        'name': 'To Do',
-    }
-    return StubResponse(fake_response_data, 200)
-
-def mock_get(url, params):
-    test_board_id = os.environ.get('TRELLO_BOARD_ID')
-
-    if url == f'https://api.trello.com/1/boards/{test_board_id}/cards': 
-        return get_cards_stub()
-    elif url.startswith('https://api.trello.com/1/lists/'): 
-        return get_list_stub()
