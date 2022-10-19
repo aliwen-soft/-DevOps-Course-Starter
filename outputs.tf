ba139c3cf79658d031f6c4c3db4c6c6a27e451d7
aliwen-soft <74604620+aliwen-soft@users.noreply.github.com>
2022-10-19 14:58:31 +0100

    Revert "M12"

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "webapp_url" {
-  value = "https://${azurerm_app_service.main.default_site_hostname}"
-}
-
-output "webhook_url" {
-  sensitive = true
-  value     = "https://${azurerm_app_service.main.site_credential[0].username}:${azurerm_app_service.main.site_credential[0].password}@${azurerm_app_service.main.name}.scm.azurewebsites.net/docker/hook"
-}
